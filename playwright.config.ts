65635f0c7c1a9e92c36b746d2ec9aa5165ebfef1
Gunn Olaussen <gunn.olaussen@gmail.com>
2026-09-16 11:03:07 +0200

    Initialize playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv';
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ */
+dotenv.config({ quiet: true });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
+    baseURL: 'https://advantageonlineshopping.com',
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'desktopF',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    // {
+    //   name: 'desktopC',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+    // {
+    //   name: 'desktopS',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    /* Test against mobile viewports. */
+    {
+      name: 'mobileS',
+      use: { ...devices['iPhone 12'] },
+    },
+    // {
+    //   name: 'mobileC',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    /* Test against branded browsers. */
+    // {
+    //   name: 'desktopE',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+  ]
+});
